13a895fbcaef39a46827eceaab9d7dd39ed803f9
wanbean <630292455@qq.com>
2021-08-28 14:18:08 +0800

    校准使用led灯语提示

--- User/main.c
+++ User/main.c
@@ -1,30 +1,3 @@
-/**
-  ******************************************************************************
-  * @file     Project/STM8L10x_StdPeriph_Templates/main.c
-  * @author   MCD Application Team
-  * @version V1.2.1
-  * @date    30-September-2014
-  * @brief    This file contains the firmware main function.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
-  *
-  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  *        http://www.st.com/software_license_agreement_liberty_v2
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
-  ******************************************************************************
-  */
-
 /* Includes ------------------------------------------------------------------*/
 #include "stm8l10x.h"
 #include "Receiver_PPM.h"
@@ -35,7 +8,6 @@
 /* Private define ------------------------------------------------------------*/
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
 
@@ -99,6 +71,8 @@ void main(void)
   PPM_Reveiver_Init();
   LightGPIO_Init();
   Motor_Init();
+  OutHOLD_INIT();
+
 //  GPIO_Init(GPIOA,GPIO_Pin_3,GPIO_Mode_Out_PP_High_Fast);
   enableInterrupts();//开启中断总开关
     
